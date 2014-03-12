7f2e9040315ff05d56bf470aa17c0d9f848da11c
Baudouin Duthoit <baud.duthoit@gmail.com>
2014-03-12 19:29:43 +0100

    Updated tests !
    Signed-off-by: Baudouin Duthoit <baud.duthoit@gmail.com>

--- src/Stubi.java
+++ src/Stubi.java
@@ -1,3 +1,14 @@
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.Graphics;
+import java.awt.Image;
+import java.io.File;
+import java.io.IOException;
+
+import javax.imageio.ImageIO;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
 
 public class Stubi {
 	private int a;
@@ -15,8 +26,40 @@ public class Stubi {
 	}
 	
 	public static void main(String [] arg){
+		// test 1
 		System.out.println("A");
 		Stubi s = new Stubi();
 		System.out.println(s);
+		
+		// window test
+		JFrame window = new JFrame();
+        window.setContentPane(new IHMImages());
+		window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		window.pack();
+		window.setLocation(300, 100);	// Place the windows in x=300 y=100 (starting from top right corner)
+		window.setVisible(true);
+	}
+}
+
+class IHMImages extends JPanel {
+	private static final long serialVersionUID = 1L;
+	Image img1, img2;
+
+	IHMImages() {
+		try {
+			img1 = ImageIO.read(new File("img/perso.jpg"));
+			img2 = ImageIO.read(new File("img/char.png"));
+		}
+		catch(IOException exc) {
+			exc.printStackTrace();
+		}
+		setPreferredSize(new Dimension(200, 200));
+		setBackground(Color.WHITE);
+	}
+
+	public void paintComponent(Graphics g) {
+		super.paintComponent(g);
+		g.drawImage(img1, 0, 0, getWidth(), getHeight(), this);
+		g.drawImage(img2, 30, 30, this);
 	}
 }
