53ff6ea052f3210401bf8fd88e377691732c98f7
Baudouin Duthoit <baud.duthoit@gmail.com>
2014-04-10 18:48:14 +0200

    Adding a generic Obstacle class
    Signed-off-by: Baudouin Duthoit <baud.duthoit@gmail.com>

--- src/game/Obstacle.java
+++ src/game/Obstacle.java
@@ -1,35 +1,32 @@
 package game;
 
-import java.awt.image.BufferedImage;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
 
-public class Obstacle {
+public class Obstacle extends genObstacle {
 	/**
 	 * This class describe an obstacle
 	 */
-	private static int sWidth = 30;
-	private static int sHeight = 30;
-	private static int confLines = Stubi.WINDY/sWidth;
-	private static SpriteSheet ss = new SpriteSheet("img/obstacles.png",sWidth,sHeight);
-	private int n;		// type of obstacle
+	
+	// Not static
 	private int x,y;	// position in the image
-	private boolean collisionTop = true;
-	private boolean collisionBot = true;
-	private boolean collisionSide = true;
-	private boolean visible = true;
-	private int hurting;
 	
 	/**
 	 * Main Constructor
-	 * @param n is the type of the Obstacle
+	 * @param key is the key to access the Obstacle, it is its description on CSV file
 	 * @param x is the X coordinate on the whole level
 	 * @param y is the Y coordinate on the whole level
-	 */
-	public Obstacle(int n, int x, int y) {
-		this.n = n;
+	 */	
+	public Obstacle(String key, int x, int y) {
+		genObstacle tmp = (genObstacle) genObstacle.obsMapping.get(key);
+		super.n = tmp.getID();
+		super.collisionTop = tmp.CollisionTop();
+		super.collisionBot = tmp.CollisionBot();
+		super.collisionSide = tmp.CollisionSide();
+		super.visible = tmp.visible();
+		super.hurting = tmp.getDmg();
 		this.x = x;
 		this.y = y;
 	}
@@ -43,39 +40,6 @@ public class Obstacle {
 		return y;
 	}
 	
-	public int getType() {
-		return n;
-	}
-	
-	public boolean CollisionTop() {
-		return collisionTop;
-	}
-	
-	public boolean visible() {
-		return visible;
-	}
-	
-	public BufferedImage getImg() {
-		return ss.getSprite(n);
-	}
-	
-	public static int getHeight() {
-		return sHeight;
-	}
-	
-	public static int getWidth() {
-		return sWidth;
-	}
-	
-	// Setters
-	public void setCollisionTop(boolean b) {
-		collisionTop = b;
-	}
-	
-	public void setVisible(boolean b) {
-		visible = b;
-	}
-	
 	// Classical functions
 	
 	public String toString() {
@@ -87,38 +51,10 @@ public class Obstacle {
 		return asw;
 	}
 	
-	// Other functions	
+	// Static functions
 	
 	/**
-	 * This function is reading a configuration file and depending on this, create an array of obstacles with the help of charToObs
-	 * @param n is the level number : each level has a different configuration
-	 * @return a bi-dimensional array of Obstacle 
-	 * @deprecated
-	*/
-	public static Obstacle[][] readConfTxt(int n){
-		String path = "/conf/confLevel"+ n +".txt";		
-		Obstacle[][] obs = new Obstacle[Obstacle.confLines][];
-		
-		InputStreamReader isReader= new InputStreamReader(Obstacle.class.getResourceAsStream(path));
-		try (BufferedReader reader = new BufferedReader(isReader)) {
-		    String line = null;
-		    int j = 0;	// y coord
-		    while ((line = reader.readLine()) != null && j<obs.length) {	// Reading the file line by line
-				ArrayList<Obstacle> tmpObs = new ArrayList<Obstacle>();	// creates an arrayList to contain the elements
-		    	for (int i = 0; i<line.length(); ++i){	// i = x coord
-		    		tmpObs.add(charToObs(line.charAt(i),i,j));
-		    	}
-		    	obs[j] = tmpObs.toArray(new Obstacle[tmpObs.size()]);	// transform the arrayList into an array
-		    	++j;
-		    }
-		} catch (IOException x) {
-		    System.err.format("IOException: %s%n", x);
-		}
-		return obs;
-	}	
-	
-	/**
-	 * This function is reading a configuration file and depending on this, create an array of obstacles with the help of StringToObs
+	 * This function is reading a configuration file and depending on this it creates an array of obstacles with the help of StringToObs
 	 * @param n is the level number : each level has a different configuration
 	 * @return a bi-dimensional array of Obstacle 
 	*/
@@ -131,79 +67,20 @@ public class Obstacle {
 		    String line = null;
 		    int j = 0;	// y coord
 		    while ((line = reader.readLine()) != null) {	// Reading the file line by line
-				ArrayList<Obstacle> tmpObs = new ArrayList<Obstacle>();	// creates an arrayList to contain the elements
+				ArrayList<Obstacle> tmpObsList = new ArrayList<Obstacle>();	// creates an arrayList to contain the elements
 				String[] split = line.split(",");
 		    	for (int i = 0; i<split.length; ++i){	// i = x coord
-		    		tmpObs.add(StringToObs(split[i],i,j));
+		    		int x = i*genObstacle.getWidth();
+		    		int y = j*genObstacle.getHeight();
+		    		Obstacle tmpObs = new Obstacle(split[i],x,y);
+		    		tmpObsList.add(tmpObs);
 		    	}
-		    	obs[j] = tmpObs.toArray(new Obstacle[tmpObs.size()]);	// transform the arrayList into an array
+		    	obs[j] = tmpObsList.toArray(new Obstacle[tmpObsList.size()]);	// transform the arrayList into an array
 		    	++j;
 		    }
 		} catch (IOException x) {
 		    System.err.format("IOException: %s%n", x);
 		}
 		return obs;
-	}
-	
-	/**
-	 * This method return an Obstacle from it's given char ID 
-	 * @param c is its ID
-	 * @param i is its position on the line (will be used to know its position on the screen)
-	 * @param j is its position on the column (will be used to know its position on the screen)
-	 * @return an Obstacle
-	 * @deprecated
-	 */
-	private static Obstacle charToObs(char c, int i, int j){
-		String s = c + "";
-		return StringToObs(s, i, j);
-	}
-	
-	/**
-	 * This method return an Obstacle from it's given String ID ("12", "grass", "soil", ...) 
-	 * @param s is its ID
-	 * @param i is its position on the line (will be used to know its position on the screen)
-	 * @param j is its position on the column (will be used to know its position on the screen)
-	 * @return an Obstacle
-	 */
-	private static Obstacle StringToObs(String s, int i, int j){
-		Obstacle asw = null;
-		switch(s){
-		case "":
-			asw = new Obstacle(10,i*sWidth,j*sHeight);
-			asw.setCollisionTop(false);
-			asw.setVisible(false);
-			break;
-		case "0":	// Wall
-			asw = new Obstacle(0,i*sWidth,j*sHeight);
-			// ...
-			break;
-		case "1":	// Stairs steps (ascending)
-			asw = new Obstacle(1,i*sWidth,j*sHeight);
-			break;
-		case "2":	// Stairs steps (flat)
-			asw = new Obstacle(2,i*sWidth,j*sHeight);
-			break;
-		case "3":	// Stairs steps (descending)
-			asw = new Obstacle(3,i*sWidth,j*sHeight);
-			break;
-		case "4":	// Stairs (content)
-			asw = new Obstacle(4,i*sWidth,j*sHeight);
-			break;
-		case "5":	// Flames
-			asw = new Obstacle(5,i*sWidth,j*sHeight);
-			break;
-		case "6":	// Flames
-			asw = new Obstacle(6,i*sWidth,j*sHeight);
-			break;
-		case "/":	// Final portal
-			asw = new Obstacle(15,i*sWidth,j*sHeight);
-			asw.setCollisionTop(false);
-			break;
-		default:
-			asw = new Obstacle(10,i*sWidth,j*sHeight);
-			asw.setCollisionTop(false);
-			asw.setVisible(false);
-		}
-		return asw;
 	}
 }
---
+++ src/game/genObstacle.java
@@ -0,0 +1,155 @@
+package game;
+
+import java.awt.image.BufferedImage;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+import javax.script.SimpleBindings;
+
+public class genObstacle {
+	/**
+	 * This class describe a generic Obstacle
+	 */
+	// Static
+	protected static int sWidth = 30;
+	protected static int sHeight = 30;
+	protected static int confLines = Stubi.WINDY/sWidth;
+	protected static SpriteSheet ss;
+	protected static String pathAttr = "/conf/obstacleAttributes.csv";
+	protected static SimpleBindings obsMapping;
+	
+	// Not static
+	protected int n;		// type of obstacle
+	protected boolean collisionTop = true;
+	protected boolean collisionBot = true;
+	protected boolean collisionSide = true;
+	protected boolean visible = true;
+	protected int hurting = 0;
+	
+	public genObstacle(){
+		n = 0;
+	}
+	
+	/**
+	 * Main constructor.
+	 * @param ID is the ID on the SpriteSheet
+	 * @param collision if the block collide or not
+	 * @param visible if the block is visible or not
+	 * @param hurting if the block is hurting or not
+	 */
+	public genObstacle(int ID, boolean collision, boolean visible, int hurting){
+		n = ID;
+		collisionTop = collision;
+		collisionBot = collision;
+		collisionSide = collision;
+		this.visible = visible;
+		this.hurting = hurting;
+	}
+
+	/**
+	 * Accurate Constructor
+	 * @param ID is the ID on the SpriteSheet
+	 * @param collisionTop if there is a collision on the top of the block
+	 * @param collisionBot if there is a collision on the bottom of the block
+	 * @param collisionSide if there is a collision on the side of the block
+	 * @param visible if the block is visible or not
+	 * @param hurting if the block is hurting or not
+	 */
+	public genObstacle(int ID, boolean collisionTop, boolean collisionBot, boolean collisionSide, boolean visible, int hurting){
+		n = ID;
+		this.collisionTop = collisionTop;
+		this.collisionBot = collisionBot;
+		this.collisionSide = collisionSide;
+		this.visible = visible;
+		this.hurting = hurting;
+	}
+
+	//Getters
+	public int getID() {
+		return n;
+	}
+	
+	public boolean CollisionTop() {
+		return collisionTop;
+	}
+	
+	public boolean CollisionBot() {
+		return collisionBot;
+	}
+	
+	public boolean CollisionSide() {
+		return collisionSide;
+	}
+	
+	public boolean visible() {
+		return visible;
+	}
+	
+	public int getDmg() {
+		return hurting;
+	}
+	
+	public BufferedImage getImg() {
+		return ss.getSprite(n);
+	}
+	
+	public static int getHeight() {
+		return sHeight;
+	}
+	
+	public static int getWidth() {
+		return sWidth;
+	}
+	
+	// Setters
+	public void setCollisionTop(boolean b) {
+		collisionTop = b;
+	}
+	
+	public void setVisible(boolean b) {
+		visible = b;
+	}
+	
+	// Static methods
+	/**
+	 * This function initialize the class by using the level number (to load the right spriteSheet)
+	 * @param n is the level number or more exactly, the spriteSheet type
+	 */
+	public static void init(int n) {
+		setMapping();
+		ss = new SpriteSheet("img/obstacles" + n + ".png",sWidth,sHeight);
+	}
+	
+	/**
+	 * this function update the spriteSheet, with a new type
+	 * @param n is the spriteSheet type
+	 */
+	public static void changeSpriteSheet(int n) {
+		ss = new SpriteSheet("img/obstacles" + n + ".png",sWidth,sHeight);		
+	}
+	
+	/**
+	 * This method is used to se a mapping between the obstacles and their attributes described in the csv file "obstacleAttributes.csv"
+	 */
+	private static void setMapping(){
+		obsMapping = new SimpleBindings();	// creates an arrayList to contain the elements
+		InputStreamReader isReader= new InputStreamReader(Obstacle.class.getResourceAsStream(pathAttr));
+		try (BufferedReader reader = new BufferedReader(isReader)) {
+		    String line = reader.readLine();	// read the first line
+		    while ((line = reader.readLine()) != null) {	// Reading the file line by line
+				String[] split = line.split(",");
+				int ID = Integer.parseInt(split[1]);
+				boolean cT = (split[2]=="1");
+				boolean cB = (split[3]=="1");
+				boolean cS = (split[4]=="1");
+				boolean v = (split[5]=="1");
+				int h = 0;
+				genObstacle tmpGenObs = new genObstacle(ID,cT,cB,cS,v,h);
+		    	obsMapping.put(split[0],tmpGenObs);
+		    }
+		} catch (IOException x) {
+		    System.err.format("IOException: %s%n", x);
+		}
+	}
+}
